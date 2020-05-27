584749f31d520bc01080f5dade099c565c0c94ee
pradhumnpanchal <pradhumn44@gmail.com>
2020-05-27 15:10:22 +0530

    Validations to the Project

--- PPMToolFullStack/src/main/java/io/pproject/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/pproject/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package io.pproject.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -8,13 +12,27 @@ public class Project{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "Project Name is Required")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is Required")
+    @Size(min =4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project Description Required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project(){
