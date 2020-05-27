e6055adc8cb2d291f52b0777f1395d89cafcd93b
pradhumnpanchal <pradhumn44@gmail.com>
2020-05-27 17:55:10 +0530

    clean code for field error map

--- PPMToolFullStack/src/main/java/io/pproject/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/pproject/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package io.pproject.ppmtool.web;
 
 import io.pproject.ppmtool.domain.Project;
+import io.pproject.ppmtool.services.MapValidationErrorService;
 import io.pproject.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,16 +25,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for(FieldError error : result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null)
+            return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
