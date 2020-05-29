63697a078131110ce3058ee5c3e549f4ebba601b
pradhumnpanchal <pradhumn44@gmail.com>
2020-05-29 18:09:16 +0530

    imported bootstrap and played with components

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 export default class Dashboard extends Component {
   render() {
-    return <h1>Welcome to the Dashboard</h1>;
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        <ProjectItem />
+      </div>
+    );
   }
 }
---
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class ProjectItem extends Component {
+  render() {
+    return <h1>Project Item</h1>;
+  }
+}
